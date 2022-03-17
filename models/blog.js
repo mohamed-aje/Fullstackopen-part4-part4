e4ff11b784f36528344c2a3e960368b68202b079
mohamed-aje <chochoaj93@gmail.com>
2022-03-17 15:24:28 +0200

    models,utils,controllers

---
+++ models/blog.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true, minlength: 5 },
+  author: { type: String, required: true, minlength: 5 },
+  url: { type: String, required: true },
+  likes: { type: Number, required: true },
+});
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+module.exports = mongoose.model("Blog", blogSchema);
